4a37f96d26405ac878c79f3a5422a708f7dd7d96
Ant4g0n1st <dragon124victor421force@gmail.com>
2020-03-11 19:12:48 -0600

    Removed testing files.

--- snmp-baseline/graphTest.py
+++
@@ -1,9 +0,0 @@
-import rrdtool
-
-rrdtool.graph( "traficoRED.png",
-                 "--start",'1582610400',
-                "--end","N",
-                 "--vertical-label=Bytes/s",
-                 "DEF:icmp=localhost/snmp.rrd:icmpInMsgs:AVERAGE",
-                 "LINE1:icmp#0000FF:ICMP in Msgs\r")
-
