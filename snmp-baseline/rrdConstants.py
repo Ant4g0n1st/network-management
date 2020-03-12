46b09d95e2a8b30868ca6ce9f9be6d565028c2fc
Ant4g0n1st <dragon124victor421force@gmail.com>
2020-03-12 04:47:23 -0600

    Graphs completed.

--- snmp-baseline/rrdConstants.py
+++ snmp-baseline/rrdConstants.py
@@ -3,10 +3,13 @@ DS_DISK = 'disk'
 DS_CPU = 'cpu'
 
 TYPE_COUNTER = 'COUNTER'
+TYPE_GAUGE = 'GAUGE'
 
 # 5-minute frequency is recommended.
-THRESHOLD = '600'
-STEP = '300'
+THRESHOLD = '10'
+STEP = '5'
+#THRESHOLD = '600'
+#STEP = '300'
 
 # Let's collect five hours.
 RRA_DEFAULT_SETTINGS = 'RRA:AVERAGE:0.5:1:60'
@@ -14,3 +17,23 @@ RRA_DEFAULT_SETTINGS = 'RRA:AVERAGE:0.5:1:60'
 UNKNOWN = 'U'
 NOW = 'N'
 
+GRAPH_HEIGHT = '320'
+GRAPH_WIDTH = '1280'
+
+MEMORY_GRAPH = 'memory.png'
+DISK_GRAPH = 'disk.png'
+CPU_GRAPH = 'cpu.png'
+
+TIME_FRAME = 300
+
+# Baseline levels
+READY = 0
+SET = 10
+GO = 20
+
+# Actual baseline
+BASELINE = {
+        DS_MEMORY : { READY : 50, SET : 60, GO : 70},
+        DS_DISK : { READY : 10, SET : 20, GO : 30},
+        DS_CPU : { READY : 30, SET : 40, GO : 50}
+    }
