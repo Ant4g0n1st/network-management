4127b01162588a1fc01b0a50a9b7e5e44de9f826
Ant4g0n1st <dragon124victor421force@gmail.com>
2020-05-12 02:39:31 -0500

    Importing missing constants.

---
+++ trap-handlers/appConstants.py
@@ -0,0 +1,26 @@
+#SNMP Version Constants according to the API.
+
+SNMP_V2C = 1
+SNMP_V1 = 0
+
+#Monitor Constants.
+
+DB_FILENAME = 'perf.rrd'
+MONITOR_FREQ = 20
+
+#Nodes to be monitored.
+
+HR_PROCESSOR_LOAD_COLUMN = '1.3.6.1.2.1.25.3.3.1.2'
+HR_STORAGE_TABLE_OID = '1.3.6.1.2.1.25.2.3'
+
+#Report Generator Constants.
+
+TEMPLATE_FILE = 'reportTemplate.html'
+
+#Baseline Thresholds
+READY = 45
+SET = 50
+GO = 60
+
+ALERT_TIMEOUT = 1800
+
