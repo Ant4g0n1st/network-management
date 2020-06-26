eb39f916496c668260e026367f375a981b1ea8d0
Ant4g0n1st <vic.noriega.am@outlook.com>
2020-06-25 21:38:15 -0500

    Fixed the bandwidth bug found in other assignment .

--- snmp-flow-control/snmpAgentMonitor.py
+++ snmp-flow-control/snmpAgentMonitor.py
@@ -15,14 +15,8 @@ class SnmpAgentMonitor(Thread):
         Thread.__init__(self)
         self.storage = SnmpMonitorStorage(snmpAgentInfo)
         self.snmpAgentInfo = snmpAgentInfo
-        self.running = True
-
-        # Information to compute deltas.
         self.previous = dict()
-
-        self.previous['time'] = int(time.time())
-        self.previous['time'] -= appConstants.MONITOR_FREQ
-
+        self.running = True
         self.start()
 
     def stop(self):
@@ -31,16 +25,34 @@ class SnmpAgentMonitor(Thread):
     def __del__(self):
         self.stop()
 
-    def query(self):
+    def run(self):
+        try:
+            # Information to compute deltas.
+            self.previous['ifInOctets'] = self.queryInterface() 
+
+            self.previous['time'] = int(time.time())
+            self.previous['time'] -= appConstants.MONITOR_FREQ
+
+            while self.running:
+                updates = self.computeBandwidth()
+
+                self.storage.updateDatabase(updates)
+
+                time.sleep(appConstants.MONITOR_FREQ) 
+        except:
+            logging.error('Exception while monitoring %s : %s',
+                self.snmpAgentInfo, sys.exc_info())
+        self.stop()
+
+    def queryInterface(self):
         # Get the device's ifTable
-        query = snmp.snmpWalk(
-                self.snmpAgentInfo.snmpVersion,
-                self.snmpAgentInfo.community,
-                self.snmpAgentInfo.address,
-                self.snmpAgentInfo.port,
+        query = snmpQuery.snmpWalk(
+                self.agent.snmpVersion,
+                self.agent.community,
+                self.agent.address,
+                self.agent.port,
                 '1.3.6.1.2.1.2.2.1'
             ) 
-        now = int(time.time())
 
         # Loop through the entries to find the first
         # interface ot type ethernetCsmacd (6)
@@ -58,45 +70,27 @@ class SnmpAgentMonitor(Thread):
         if not ifIndex:
             return None
         
-        # Compute average speed using deltas
-        # and a formula similar to Cisco's.
-        ifOutOctets = int(query['1.3.6.1.2.1.2.2.1.16.' + ifIndex])
         ifInOctets = int(query['1.3.6.1.2.1.2.2.1.10.' + ifIndex])
 
-        if not 'ifOutOctets' in self.previous:
-            self.previous['ifOutOctets'] = ifOutOctets
+        return ifInOctets
 
-        if not 'ifInOctets' in self.previous:
-            self.previous['ifInOctets'] = ifInOctets
+    def computeBandwidth(self):
+        # Compute average speed using deltas
+        # and a formula similar to Cisco's.
+        ifInOctets = self.queryInterface()
+        now = int(time.time())
 
-        deltaOut = ifOutOctets - self.previous['ifOutOctets']
+        if not ifInOctets:
+            return None
+
         deltaIn = ifInOctets - self.previous['ifInOctets']
         deltaTime = now - self.previous['time']
 
-        outBandwidth = (deltaOut * 8) / deltaTime
         inBandwidth = (deltaIn * 8) / deltaTime
         
         # Store the current values for the next iteration.
-        self.previous['ifOutOctets'] = ifOutOctets
         self.previous['ifInOctets'] = ifInOctets
         self.previous['time'] = now
     
-        return {
-                appConstants.DS_OUTBW : outBandwidth,
-                appConstants.DS_INBW : inBandwidth
-            }
-
-    def run(self):
-        while self.running:
-            try:
-                updates = self.query()
-
-                self.storage.updateDatabase(updates)
-
-                time.sleep(appConstants.MONITOR_FREQ) 
-
-            except:
-                logging.error('Exception while monitoring %s : %s',
-                    self.snmpAgentInfo, sys.exc_info())
-                self.stop()
+        return inBandwidth
 
